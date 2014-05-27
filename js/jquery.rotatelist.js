5917c316fcfd9e3db8234544d13d7929ca859501
Valdinei Reis <valdineimtz@gmail.com>
2014-05-27 18:27:36 -0300

    atualização do script

--- js/jquery.rotatelist.js
+++ js/jquery.rotatelist.js
@@ -6,8 +6,9 @@
 *
 * Licensed under The MIT License
 *
-* @version 0.2.0
+* @version 0.2.1
 * @since 21.05.2014
+* @lastUpdate 27.05.2014
 * @author Valdinei Reis da Silva
 * @documentation valdineireis.com.br
 * @twitter twitter.com/valdineireis
@@ -16,7 +17,7 @@
 *
 * Usage with default values:
 * ---------------------------------------------------------------------------------
-* $.rotateList('table#rotated tbody tr');
+* $('table#rotated tbody').rotateList();
 *
 * <table id="rotated">
 * 	<thead>
@@ -50,133 +51,119 @@
 * </table>
 */
 ;(function($){
-    $.rotateList = function(selector, settings) {
-        var lista           = $(selector),
-            started         = 0,
+    $.fn.rotateList = function(settings) {
+        var opt	            = $.extend({}, $.fn.rotateList.defaults, settings),
+            self            = this,
+            trs             = self.children(),
+            started         = false,
             counter         = 0,
-            timerOn         = 0,
-            rotationNumber  = 0, /*  */
-            startedFunction = false; /* veirica se a função externa foi iniciada */
+            rotationNumber  = 0,
+            startedFunction = false;
         
-        var config = {
-            minLines           : 5, /* quantidade minima de linhas para rotação */
-            speed              : 2000, /* velocidade da rotação */
-            colorSelectedRow   : '#F1F1F1', /* cor da linha selecionada */
-            colorBackgroundRow : '#FFFFFF', /* cor de fundo das linha */
-            colorText          : '#444444', /* cor do texto */
-            colorSelectedText  : '#000000', /* cor do texto da linha selecionada */
-            externalFunction   : null, /* inicia uma função externa ao concluir rotação */
-            stopOnComplete     : false /* parar ao completar rotação */
+        var listSize = function() {
+            return trs.size();
+        }, setListColor = function() {
+            // cor de fundo e do texto
+            trs.css({
+                'background-color': opt.colorBackgroundRow,
+                'color': opt.colorText
+            });
+        }, startStop = function() {
+			if(!started) startScroll();
+			else stopScroll();
+		}, startScroll = function() {
+			if(started) {
+				return;
+			} else {
+				// verifica a quantidade minima de linha para rotacionar
+				if(listSize() >= opt.minLines) {
+					scrollUp();
+					started = true;
+				} else {
+					// chama a função externa, caso tenha
+					startExternalFunction();
+				}
+			}
+		}, stopScroll = function() {
+			if(started) {
+				clearInterval(counter);
+				started = false;
+			}
+		}, updateColorLine = function(linha) {
+			// cor da linha atual
+			linha.css({
+				'background-color' : opt.colorBackgroundRow, 
+				'color' : opt.colorText
+			});
+			
+			// cor da próxima linha
+			linha.next().css({
+				'background-color' : opt.colorSelectedRow, 
+				'color' : opt.colorSelectedText
+			});
+        }, isFirstScrollUpComplete = function() {
+            if(listSize() == rotationNumber) 
+                return true;
+            else 
+                return false;
+        }, verifyStopOnComplete = function() {
+			if(opt.stopOnComplete && isFirstScrollUpComplete()) {
+				startStop();
+			}
+		}, startExternalFunction = function() {
+			if(!startedFunction) {
+                if(isFirstScrollUpComplete()) {
+				    if(opt.externalFunction) opt.externalFunction();
+				    startedFunction = true;
+                }
+			}
+		}, scrollUp = function() {
+            counter = setInterval(function(){
+                updateColorLine(trs.first());
+                
+                trs.slice(20).hide();
+                trs.filter(':hidden').eq(0).slideUp();
+                
+                trs.eq(0).slideDown(function(){
+                    $(this).appendTo(self);
+                    trs = self.children();
+                });
+                
+                rotationNumber++;
+                
+                verifyStopOnComplete();
+                
+                startExternalFunction();
+            },opt.speed);
         };
         
-        if (settings){$.extend(config, settings);}
-        
-        function listSize() {
-            return lista.size();
-        }
-        
-        // cor de fundo e do texto
-        function setListColor() {
-            lista.parent().css({
-                'background-color': config.colorBackgroundRow,
-                'color': config.colorText
-            });
-        }
-        
-        function startStop() {
-            if(started === 0) {
-                startRolagem();
-                started = 1;
-            } 
-            else if(started === 1)  {
-                stopRolagem();
-                started = 0;
-            }
-        }
-        
-        function startRolagem() {
-            if(timerOn == 1) {
-                return;
-            } else {
-                // verifica a quantidade minima de linha para rotacionar
-                if(listSize() >= config.minLines) {
-                    counter = setInterval(rolaLista, config.speed);
-                    timerOn = 1;
-                } else {
-                    // chama a função externa, caso tenha
-                    startExternalFunction();
-                }
-            }
-        }
-        
-        function stopRolagem() {
-            if(timerOn == 1) {
-                clearInterval(counter);
-                timerOn = 0;
-            }
-        }
-        
-        function rolaLista() {
-            var itens = $(selector),
-                prev = $(itens).first();
-            
-            updateColorLine(prev);
-            
-            $.unique(prev).each(function(i) {
-                $(this).delay(i*600).slideUp(function() {
-                    $(this).appendTo(this.parentNode).slideDown();
-                });
-            });
-            
-            rotationNumber++;
-            
-            if(listSize() == rotationNumber) {
-                startExternalFunction();
-                rotationNumber = 0;
-            }
-            
-            verifyStopOnComplete();
-        }
-        
-        function updateColorLine(linha) {
-            // cor da linha atual
-            linha.css({
-                'background-color' : config.colorBackgroundRow, 
-                'color' : config.colorText
-            });
-            
-            // cor da próxima linha
-            linha.next().css({
-                'background-color' : config.colorSelectedRow, 
-                'color' : config.colorSelectedText
-            });
-        }
-        
-        function verifyStopOnComplete() {
-            if(listSize() == rotationNumber && config.stopOnComplete) {
+        self.mouseenter(function(){
+            if(started) {
                 startStop();
+                $(this).css('cursor', 'wait');
             }
-        }
-        
-        function startExternalFunction() {
-            if(startedFunction == false) {
-                if(config.externalFunction) config.externalFunction();
-                startedFunction = true;
+        }).mouseleave(function(){
+            if(!started) {
+                startStop();
+                $(this).css('cursor', 'default');
             }
-        }
-        
-        lista.mouseenter(function(){
-            startStop();
-            $(this).css('cursor', 'wait');
-        })
-        .mouseleave(function(){
-            startStop();
-            $(this).css('cursor', 'default');
         });
         
         setListColor();
         startStop();
         
         return this;
-    };
+    }
+    
+    $.fn.rotateList.defaults = {
+        minLines           : 5, /* quantidade minima de linhas para rotação */
+        speed              : 2000, /* velocidade da rotação */
+        colorSelectedRow   : '#F1F1F1', /* cor da linha selecionada */
+        colorSelectedText  : '#000000', /* cor do texto da linha selecionada */
+        colorBackgroundRow : '#FFFFFF', /* cor de fundo das linha */
+        colorText          : '#444444', /* cor do texto */
+        externalFunction   : null, /* inicia uma função externa ao concluir rotação */
+        stopOnComplete     : false /* parar ao completar rotação */
+    }
+    
 })(jQuery);
